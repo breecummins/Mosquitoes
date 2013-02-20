2a0bbd998727ce42905ce0cd44e7e9ba37f58e0d
Bree Cummins <breecummins@gmail.com>
2013-02-20 12:20:06 -0700

    map and for loop interpolation methods tested and working

--- Mosquitoes/interpFunctions.py
+++ Mosquitoes/interpFunctions.py
@@ -26,7 +26,7 @@ def interpFromGridWithMaps(xy,h,randVel1,randVel2,CO2):
     Vij = map(lambda matinds: map(lambda m: (randVel1[m], randVel2[m], CO2[m]),matinds),[ij,[(p,k+1) for (p,k) in ij],[(p+1,k) for (p,k) in ij], [(p+1,k+1) for (p,k) in ij]])
 
     def interpolate(ind):
-        return [nodes[k][0]*Vij[0][k][ind] + nodes[k][1]*Vij[1][k][ind] + nodes[k][2]*Vij[2][k][ind] + nodes[k][3]*Vij[3][k][ind] for k in range(len(Vij[0]))]
+        return [nodes[k][0]*Vij[0][k][ind] + nodes[k][1]*Vij[1][k][ind] + nodes[k][2]*Vij[2][k][ind] + nodes[k][3]*Vij[3][k][ind] for k in range(len(xy))]
     
     # perform the interpolation
     ur = interpolate(0)
@@ -35,32 +35,43 @@ def interpFromGridWithMaps(xy,h,randVel1,randVel2,CO2):
 
     return ur,vr,c
 
-if __name__ == '__main__':
-    # get simulation parameters
-    import numericalSims as nS
-    mysim = nS.numericalSims()
-    matsize = mysim.xg.shape
-    # stub for testing, these values will be filled in during a simulation
-    randVel1 = 0.03*mysim.xg + 0.1
-    randVel2 = -0.02*mysim.yg
-    CO2 = mysim.xg + mysim.yg 
-    xy = [(48.32,5.02),(16.94,34.43),(69.50,90.98)]
-    # exact values at these locations
-    ur_exact = [0.03*z[0] + 0.1 for z in xy]
-    vr_exact = [-0.02*z[1] for z in xy]
-    c_exact = [z[0] + z[1] for z in xy] 
-    # call interpolation functions
-    ur,vr,c=interpFromGridWithMaps(xy,mysim.simsParams['h'],randVel1,randVel2,CO2)
-    print('ur_exact:')
-    print(ur_exact)
-    print('ur:')
-    print(ur)
-    print('vr_exact:')
-    print(vr_exact)
-    print('vr:')
-    print(vr)
-    print('c_exact:')
-    print(c_exact)
-    print('c:')
-    print(c)
+def interpFromGridSingleForLoop(xy,h,randVel1,randVel2,CO2):
+    '''
+    This function interpolates values located at grid nodes to the 
+    locations (x,y). 
+    xy is a list of (x,y) tuples. 
+    h is (scalar) grid spacing.
+    randVel* and CO2 are velocity and CO2 values on the grid.
+
+    '''
+    def interpolate(ind):
+        return nodes[0]*Vij[0][ind] + nodes[1]*Vij[1][ind] + nodes[2]*Vij[2][ind] + nodes[3]*Vij[3][ind]
+
+    ur = []
+    vr = []
+    c = []
+        
+    for z in xy:
+        # Find indices of the closest node to (x,y) TO THE LOWER LEFT assuming 
+        # a cell-centered grid with lower left corner of the domain located at 
+        # (0,0); i.e. lowest left-most grid point in the domain is (h/2, h/2).
+        # Note that int always rounds toward zero.
+        ij = (int(z[0]/h - 0.5), int(z[1]/h - 0.5))
+
+        # find the remainders to do the interpolation
+        rxy = (z[0]/h - 0.5 - ij[0], z[1]/h -0.5 - ij[1])
+        
+        # Find the proportion of the value at each node that contributes to the 
+        # interpolation at (x,y). nodes = (lowerleft, lowerright, upperleft, upperright)
+        nodes = ((1-rxy[0])*(1-rxy[1]), (1-rxy[0])*rxy[1], rxy[0]*(1-rxy[1]), rxy[0]*rxy[1] )
+
+        # get the values of the CO2 and random wind at the four closest nodes
+        Vij = map(lambda m: (randVel1[m], randVel2[m], CO2[m]),[ij,(ij[0],ij[1]+1),(ij[0]+1,ij[1]),(ij[0]+1,ij[1]+1)])
+
+        # perform the interpolation
+        ur.append(interpolate(0))
+        vr.append(interpolate(1))
+        c.append(interpolate(2))
+
+    return ur,vr,c
 
---
+++ Mosquitoes/unittests_interpFunctions.py
@@ -0,0 +1,31 @@
+import numericalSims as nS
+import interpFunctions as iF
+
+xy = [(48.32,5.02),(16.94,34.43),(69.50,90.98)]
+
+def testaccuracy():
+    mysim = nS.numericalSims()
+    # bilinear/linear/affine functions should be recovered exactly
+    randVel1 = 0.03*mysim.xg + 0.1
+    randVel2 = -0.02*mysim.yg
+    CO2 = mysim.xg + 2.5*mysim.yg 
+    # exact values at (x,y) locations
+    ur_exact = [0.03*z[0] + 0.1 for z in xy]
+    vr_exact = [-0.02*z[1] for z in xy]
+    c_exact = [z[0] + 2.5*z[1] for z in xy] 
+    # call interpolation functions and calculate error
+    ur,vr,c=iF.interpFromGridWithMaps(xy,mysim.simsParams['h'],randVel1,randVel2,CO2)
+    mapserr = [[abs(ur[k]-ur_exact[k]) for k in range(len(xy))],[abs(vr[k]-vr_exact[k]) for k in range(len(xy))],[abs(c[k]-c_exact[k]) for k in range(len(xy))]]
+    print('Error in maps method:')
+    print(max(max(mapserr)))
+    ur,vr,c=iF.interpFromGridSingleForLoop(xy,mysim.simsParams['h'],randVel1,randVel2,CO2)
+    forlooperr=[[abs(ur[k]-ur_exact[k]) for k in range(len(xy))],[abs(vr[k]-vr_exact[k]) for k in range(len(xy))],[abs(c[k]-c_exact[k]) for k in range(len(xy))]]
+    print('Error in for loop method:')
+    print(max(max(forlooperr)))
+
+def testspeed():
+    pass
+
+if __name__ == '__main__':
+    testaccuracy()
+    testspeed()
